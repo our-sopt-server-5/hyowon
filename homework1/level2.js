17bc71d662dec0ef127ecf1396418b006d46e4c8
Jeong-Hyowon <dndwls9252@naver.com>
2020-04-18 23:39:15 +0900

    commit hw1

---
+++ homework1/level2.js
@@ -0,0 +1,12 @@
+var server5 = [
+    {name: '권세희', age: 23, grade: 'YB', part: 'server'},
+    {name: '김보배', age: 26, grade: 'YB', part: 'server'},
+    {name: '오태진', age: 24, grade: 'OB', part: 'server'},
+    {name: '정효원', age: 24, grade: 'YB', part: 'server'},
+    {name: '허정민', age: 24, grade: 'OB',part: 'server'}
+];
+
+server5.forEach(
+    server5 => console.log(server5.name+'는(은) 나이가 '+server5.age+'살이고, ' + server5.grade+ " " +server5.part+'파트 입니다!')
+);
+
